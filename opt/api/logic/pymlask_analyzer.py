6f5bfc0d58b1ef6449f8193b10f89fb7d35b125c
sn0wEasy <yel.bocsmile@gmail.com>
2022-03-15 02:45:37 +0900

    [update] add logic

--- opt/api/logic/pymlask_analyzer.py
+++ opt/api/logic/pymlask_analyzer.py
@@ -1,6 +1,16 @@
+from mlask import MLAsk
+from tqdm import tqdm
+import re
+
 class PyMLAskAnalyzer:
+
     def __init__(self):
-        pass
+        self.emotion_analyzer = MLAsk()
 
     def pymlask_analyzer(self, tweets):
-        pass
+        
+        for tweet in tqdm(tweets):
+            text = re.sub('\n', ' ', tweet.text)
+            result = self.emotion_analyzer.analyze(text)
+
+        return result
