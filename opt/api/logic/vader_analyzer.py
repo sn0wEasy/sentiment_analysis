e34fbc2ff521b4611469066fa2583de1aa87d637
sn0wEasy <yel.bocsmile@gmail.com>
2022-03-15 02:43:49 +0900

    [fix] except

--- opt/api/logic/vader_analyzer.py
+++ opt/api/logic/vader_analyzer.py
@@ -2,6 +2,7 @@ import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from googletrans import Translator
+from tqdm import tqdm
  
 
 class VaderAnalyzer:
@@ -10,13 +11,13 @@ class VaderAnalyzer:
         tr = Translator()
         vader_analyzer = SentimentIntensityAnalyzer()
         neg, neu, pos, compound = 0, 0, 0, 0
-        for tweet in tweets:
-            print("===============================")
-            print(tweet['text'])
+        for tweet in tqdm(tweets):
+            # 英語ツイート時にNoneが返されるので無視する
             try:
                 text_en = tr.translate(tweet['text'], src="ja", dest="en").text
             except Exception as e:
                 print(e)
+                continue
 
             #VADERによる感情分析
             analyzed = vader_analyzer.polarity_scores(text_en)
